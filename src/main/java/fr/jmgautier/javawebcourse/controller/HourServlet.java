cb95c598972cbac3f0994b99f7ccca081c0d2289
Gautier <jm.gautier@bloom-multimedia.fr>
2020-07-08 13:15:15 +0200

    initial commit

---
+++ src/main/java/fr/jmgautier/javawebcourse/controller/HourServlet.java
@@ -0,0 +1,19 @@
+package fr.jmgautier.javawebcourse.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class HourServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>"+new Date() +"</h1>");
+    }
+}
